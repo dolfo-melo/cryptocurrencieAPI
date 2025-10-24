8863788880c916d6e89a013d6c3151cdd8117e61
Rodolfo Melo <rodolfogameroficial@gmail.com>
2025-10-24 07:28:53 -0300

    Update main.js

--- apps.js
+++ apps.js
@@ -1,8 +1,52 @@
-//freecurrencyapi
-const apiKey = 'fca_live_ZS71eOBRfsh29jHELTYbm1wz4AtBIFeEM3in3EeW'
+// Conectando Coinranking API
+const apiKey = 'coinrankinga46e773fb414f28a1514ada263a52916dea95defc6cf1cf6'
+const urlBase = 'https://api.coinranking.com/v2/coins/'
+var retorno = null
 
-async function capturarDados(){
-    let dados = await fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`)
-        .then(response => response.json)
-        console.log(dados)
+// Método para conectar a API
+const options = {
+    method: 'GET',
+    headers: {   
+        'Content-Type' : 'aplication.json',
+        'x-access-token': `${apiKey}`,
+        'Acess-Control-Allow-Origin' : '*'
+    }
 }
+
+// Incializando a Api
+async function conectarApi() {
+    retorno = await fetch(`${urlBase}`,options)
+        .then((response) => {
+            if(response.ok) {
+                response.json().then((json) => {
+                    console.log(json.data.coins)
+
+                    let coinsData = json.data.coins
+
+                    if(coinsData.lenght > 0){
+                        var cryptoCoins = ""
+                    }
+
+                    // For Loop
+                    coinsData.forEach((coins) => {
+                        cryptoCoins += "<tr>"
+                        cryptoCoins += `<th scope="row">${coins.rank}</th>`
+                        cryptoCoins += `<td><img src=${coins.iconUrl} width="50px" height="50px"></td>`
+                        cryptoCoins += `<td>${coins.name}</td>`
+                        cryptoCoins += `<td>${Math.fround(coins.price)} $</td>`
+                        cryptoCoins += `<td>${coins.symbol}</td>`
+                        cryptoCoins += `<td>${Math.fround(coins.marketCap)} $</td>`;"</td>";
+                    })
+                        document.getElementById('data').innerHTML = cryptoCoins
+                })
+            }
+        })
+        .catch((error) => {console.log(error)})
+    
+    // preenchimentoDados(retorno)
+}
+conectarApi()
+
+// function preenchimentoDados(retorno){
+//     console.log(retorno.)
+// }
