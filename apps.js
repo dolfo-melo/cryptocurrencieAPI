fab1021e3c55ab0854bb64e3c42f8f9ab795ecc0
Rodolfo Melo <rodolfogameroficial@gmail.com>
2025-10-24 01:44:08 -0300

    Update main.js

--- apps.js
+++ apps.js
@@ -1,1 +1,8 @@
+//freecurrencyapi
+const apiKey = 'fca_live_ZS71eOBRfsh29jHELTYbm1wz4AtBIFeEM3in3EeW'
 
+async function capturarDados(){
+    let dados = await fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`)
+        .then(response => response.json)
+        console.log(dados)
+}
